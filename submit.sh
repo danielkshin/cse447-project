30f1caca8e9dfe5b2d9a15f852d75265fda95a7f
Daniel Shin <140947149+danielkshin@users.noreply.github.com>
2026-01-23 12:58:04 -0800

    Update team member details

--- submit.sh
+++ submit.sh
@@ -6,7 +6,7 @@ rm -rf submit submit.zip
 mkdir -p submit
 
 # submit team.txt
-printf "Victor Zhong,vzhong\nNoah Smith,nasmith\nSanta Claus,sclaus" > submit/team.txt
+printf "Daniel Shin,dkmshin\nGiang Dang,giangd\nSophie Tien,sjytien" > submit/team.txt
 
 # train model
 python src/myprogram.py train --work_dir work
